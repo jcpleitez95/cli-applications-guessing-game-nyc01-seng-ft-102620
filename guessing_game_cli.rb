55641682c9c4e085c5bc11a397df4463d9b77ef0
Juan Carlos Pleitez <jcpleitez@outlook.com>
2020-10-15 22:40:15 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,6 @@ def run_guessing_game
   computer_number = rand 1..6
   prompt = "Guess a number between 1 and 6!"
   user_input = gets.chomp
-  
   prompt
   if user_input == "exit"
     puts "Goodbye!"
